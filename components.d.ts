ed34f963958ca91e79b63a14015808a197353f47
Vojtech Nadvornik <vojtech.nadvornik@communardo.de>
2025-05-05 15:11:04 +0200

    modifed overview

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Badge: typeof import('primevue/badge')['default']
     BestSellingWidget: typeof import('./src/components/dashboard/BestSellingWidget.vue')['default']
     Button: typeof import('primevue/button')['default']
     Calendar: typeof import('primevue/calendar')['default']
