cb6bc6af44827dce0e3c0335218c4c02645f24a6
Vojtech Nadvornik <vojtech.nadvornik@communardo.de>
2025-05-21 08:39:51 +0200

    minor changes

--- src/stores/permitStore.ts
+++ src/stores/permitStore.ts
@@ -1,82 +1,99 @@
-import { defineStore, storeToRefs } from 'pinia';
-import { ref, computed } from 'vue';
-import { Zone } from '@/service/ZoneService';
+import { defineStore, storeToRefs } from "pinia";
+import { ref, computed } from "vue";
+import { Zone } from "@/service/ZoneService";
 import { useUserStore } from "./user";
 
 const userStore = useUserStore();
 
 const { street, houseNumber, city } = storeToRefs(userStore);
 
-export const usePermitStore = defineStore('permit', () => {
+export const usePermitStore = defineStore("permit", () => {
+    const tomorrow = ref(new Date());
+    tomorrow.value.setDate(tomorrow.value.getDate() + 1);
 
-  const tomorrow = ref(new Date());
-  tomorrow.value.setDate(tomorrow.value.getDate() + 1);
+    const carRegistration = ref("");
 
-  const carRegistration = ref('');
+    const startDate = ref<Date>(tomorrow.value);
+    startDate.value.setDate(startDate.value.getDate() + 1);
+    const endDate = computed(() => {
+        if (!startDate.value) return null;
+        const start = new Date(startDate.value);
+        if (selectedDuration.value === "quarter") {
+            start.setMonth(start.getMonth() + 3);
+        } else if (selectedDuration.value === "year") {
+            start.setFullYear(start.getFullYear() + 1);
+        }
+        return start;
+    });
 
-  const startDate = ref<Date>(tomorrow.value);
-  startDate.value.setDate(startDate.value.getDate() + 1);
-  const endDate = computed(() => {
-    if (!startDate.value) return null;
-    const start = new Date(startDate.value);
-    if (selectedDuration.value === 'quarter') {
-      start.setMonth(start.getMonth() + 3);
-    } else if (selectedDuration.value === 'year') {
-      start.setFullYear(start.getFullYear() + 1);
-    }
-    return start;
-  });
+    const zones = ref<Zone[]>([]);
 
-  const zones = ref<Zone[]>([]);
+    const selectedZones = ref<Zone[]>([]);
+    const selectedDuration = ref("year");
+    const selectedPayment = ref("online");
 
-  const selectedZones = ref<Zone[]>([]);
-  const selectedDuration = ref('year');
-  const selectedPayment = ref('online');
+    const totalPrice = computed(() => {
+        let result = 0;
+        if (selectedZones.value) {
+            selectedZones.value.forEach((zone) => {
+                const homeZone = isHomeZone(zone);
 
-  const totalPrice = computed(() => {
-    let result = 0;
-    if (selectedZones.value) {
-        selectedZones.value.forEach((zone) => {
-            const homeZone = isHomeZone(zone);
+                if (selectedDuration.value === "quarter") {
+                    result += homeZone
+                        ? zone.pricePerQuarterWithDiscount
+                        : zone.pricePerQuarter;
+                } else if (selectedDuration.value === "year") {
+                    result += homeZone
+                        ? zone.pricePerYearWithDiscount
+                        : zone.pricePerYear;
+                }
+            });
+        }
+        return result;
+    });
 
-            if (selectedDuration.value === "quarter") {
-                result += homeZone ? zone.pricePerQuarterWithDiscount : zone.pricePerQuarter;
-            } else if (selectedDuration.value === "year") {
-                result += homeZone ? zone.pricePerYearWithDiscount : zone.pricePerYear
-            }
-        });
-    }
-    return result;
-});
+    const isHomeZone = (zone: Zone) => {
+        const addresses = zone.adresses.filter(
+            (x) => x.street == street.value && "Děčín" == city.value,
+        );
+        if (addresses.length === 0) {
+            return false;
+        }
+        const address = addresses[0];
+        if (address.numbers.length === 0) {
+            return true;
+        }
 
+        return address.numbers.filter((x) => x == houseNumber.value).length > 0;
+    };
 
-  const isHomeZone = (zone: Zone) => {
-    const addresses = zone.adresses.filter(x => x.street == street.value && "Děčín" == city.value);
-    if (addresses.length === 0) {
-        return false;
-    }
-    const address = addresses[0];
-    if (address.numbers.length === 0) {
-        return true;
-    }
+    const updateZones = (newZones: Zone[]) => {
+        zones.value = newZones;
+    };
 
-    return address.numbers.filter(x => x == houseNumber.value).length > 0;
-};
+    const setPermitData = (
+        newCarRegistration: string,
+        newStartDate:Date,
+        newSelectedZones: Zone[],
+        newPermitDuration: string
+    ) => {
+        carRegistration.value = newCarRegistration;
+        startDate.value = newStartDate;
+        selectedZones.value = newSelectedZones;
+        selectedDuration.value = newPermitDuration;
+    };
 
-  const updateZones = (newZones: Zone[]) => {
-    zones.value = newZones;
-  };
-
-  return {
-    tomorrow,
-    carRegistration,
-    startDate,
-    endDate,
-    zones,
-    selectedZones,
-    selectedDuration,
-    totalPrice,
-    selectedPayment,
-    updateZones,
-  };
+    return {
+        tomorrow,
+        carRegistration,
+        startDate,
+        endDate,
+        zones,
+        selectedZones,
+        selectedDuration,
+        totalPrice,
+        selectedPayment,
+        updateZones,
+        setPermitData,
+    };
 });
--- src/views/pages/Crud.vue
+++ src/views/pages/Crud.vue
@@ -9,7 +9,14 @@ import { Form } from 'vee-validate';
 import * as yup from "yup";
 import { Field } from 'vee-validate';
 const toast = useToast();
+import { usePermitStore } from '@/stores/permitStore';
 
+import { useRouter } from 'vue-router'; 
+import Button from 'primevue/button';
+const router = useRouter(); 
+const permitStore = usePermitStore();
+
+const { setPermitData } = permitStore;
 onMounted(() => {
     PermitService.getParkingPermits().then((data) => (products.value = data));
 });
@@ -26,7 +33,7 @@ const selectedRow = ref(null);
 
 function openCarRegDialog(row) {
     selectedRow.value = row;
-    carRegistration.value = row.carRegistration;
+    carRegistration.value = row.carregistration;
     showCarRegDialog.value = true;
 }
 
@@ -37,7 +44,8 @@ const validationSchema = yup.object().shape({
         .matches(
             /^[a-zA-Z0-9]{1,10}$/,
             "SPZ je v nesprávném formátu (max 10 znaků) a může obsahovat pouze písmena a číslice",
-        ).test('exists', 'Tato SPZ již má platné oprávnění.', async (value) => {
+        )
+        .test('exists', 'Tato SPZ již má platné oprávnění.', async (value) => {
             if (!value) return true;
             try {
                 const permits = await PermitService.getParkingPermits(value.trim());
@@ -49,28 +57,23 @@ const validationSchema = yup.object().shape({
 
 });
 
-function saveCarReg() {
+async function saveCarReg() {
     if (selectedRow.value) {
-        PermitService.updatePermit(selectedRow.value.id, carRegistration.value)
-            .then(() => {
-                selectedRow.value.carregistration = carRegistration.value;
-                // Import Toast service at the top of script section
-
-                // In the saveCarReg function, add this toast notification
-                toast.add({
-                    severity: 'success',
-                    summary: 'Úspěch',
-                    detail: 'Poznávací značka byla úspěšně aktualizována',
-                    life: 5000
-                });
-                showCarRegDialog.value = false;
-            })
-            .catch((error) => {
-                console.error("Error updating car registration:", error);
-            });
+        await PermitService.updatePermit(selectedRow.value.id, carRegistration.value)
+        selectedRow.value.carregistration = carRegistration.value;
+        toast.add({
+            severity: 'success',
+            summary: 'Úspěch',
+            detail: 'Poznávací značka byla úspěšně aktualizována',
+            life: 5000
+        });
+        showCarRegDialog.value = false;
+    } else {
+        showCarRegDialog.value = false;
     }
-    showCarRegDialog.value = false;
 }
+
+
 
 const toStatus = (status) => {
     switch (status) {
@@ -82,6 +85,11 @@ const toStatus = (status) => {
             return status;
     }
 };
+
+function copyPermit(row) {
+    setPermitData(row.carregistration, row.validFrom, row.selectedZones, row.permitDuration);
+    router.push('/edit')
+}
 
 </script>
 
@@ -95,7 +103,7 @@ const toStatus = (status) => {
                 </template>
             </Toolbar>
 
-            <DataTable ref="dt" v-model:selection="selectedProducts" :value="products" dataKey="id" :rows="10">
+            <DataTable ref="dt" :value="products" dataKey="id" :rows="10">
                 <template #header>
                     <div class="flex flex-wrap gap-2 items-center justify-between">
                         <h4 class="m-0">Vaše parkovací oprávnění</h4>
@@ -126,7 +134,7 @@ const toStatus = (status) => {
                 </Column>
                 <Column :exportable="false" style="min-width: 12rem">
                     <template #body="slotProps">
-                        <Button class="mr-2" @click="editProduct(slotProps.data)"
+                        <Button @click="copyPermit(slotProps.data)" class="mr-2" 
                             :disabled="new Date(slotProps.data.validTo) > new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)">Prodloužit
                             platnost</Button>
                         <Button class="mr-2" @click="openCarRegDialog(slotProps.data)">Změnit poznávací značku</Button>
@@ -137,9 +145,11 @@ const toStatus = (status) => {
 
         <Dialog v-model:visible="showCarRegDialog" header="Změnit poznávací značku" :modal="true" :closable="true">
             <div class="flex flex-col gap-4">
-                <Form :validation-schema="validationSchema" v-slot="{ errors }" @submit="saveCarReg" class="card">
+                <Form :validation-schema="validationSchema" v-slot="{ errors, isValidating, isSubmitting }" @submit="saveCarReg" class="card">
                     <div class="grid grid-cols-12 gap-2">
-                        <label for="name3" class="flex items-center col-span-12 mb-2 md:col-span-2 md:mb-0">Nová poznávací značka</label>
+                        <label for="name3" class="flex items-center col-span-12 mb-2 md:col-span-2 md:mb-0">Nová
+                            poznávací
+                            značka</label>
                         <div class="col-span-12 md:col-span-10">
                             <Field v-model="carRegistration" id="carRegistration" name="carRegistration" :as="InputText"
                                 :class="{ 'p-invalid': errors.carRegistration }" />
@@ -147,12 +157,12 @@ const toStatus = (status) => {
                         </div>
                     </div>
                     <div class="flex gap-2 mt-4">
-                        <Button type="submit" label="Uložit" />
+                        <Button type="submit" label="Uložit" :as="Button"
+                            :disabled=" isSubmitting || isValidating || Object.keys(errors).length > 0 " />
                         <Button label="Zrušit" @click="showCarRegDialog = false" severity="secondary" />
                     </div>
                 </Form>
             </div>
         </Dialog>
-
     </div>
 </template>
